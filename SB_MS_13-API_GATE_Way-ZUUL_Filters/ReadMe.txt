2116c95a97fdf73110120bbe9a68393d6713a5e5
Tushar <Tushar@Tushar.mshome.net>
2022-03-01 16:08:44 +0530

    API GateWay - ZUUL Filters

---
+++ SB_MS_13-API_GATE_Way-ZUUL_Filters/ReadMe.txt
@@ -0,0 +1,14 @@
+Run SB_MS_01-R_D-Server-Eureka-Cfg  project as Spring Boot Application and check Eureka server console at below url.
+http://localhost:8761
+
+follow ReadMe.txt of : SB_MS_03-MS01-BankPaymentService
+
+Run this project as Spring Boot Application refresh the Eureka server console
+Check service instance is updated with this microservice or not and test the service in using below url:
+http://localhost:8990/orders/start 
+and check response message by calling service again and again.
+
+Run SB_MS_13-API_GATE_Way-ZUUL_Filters as Spring Boot Application and check Eureka server console
+and test the GATE-Way Server at below url:
+http://localhost:9222/Tushar-api/orders/start/
+and observer the logs of PRE/POST/ROUTE Filter and ERROR filter if any error raised.
---
+++ SB_MS_13-API_GATE_Way-ZUUL_Filters/src/main/java/com/tushar/filters/Error_ZUUL_Filter.java
@@ -0,0 +1,47 @@
+package com.tushar.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class Error_ZUUL_Filter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		System.out.println("Error_ZUUL_Filter.shouldFilter()");
+		// enable the filter by returning true
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		System.out.println("Error_ZUUL_Filter.run()");
+		RequestContext context= RequestContext.getCurrentContext();//access to other objects
+		HttpServletRequest req = context.getRequest(); 
+		System.out.println("Error_ZUUL_Filter.run(getContentType)  "+req.getContentType());
+		System.out.println("Error_ZUUL_Filter.run(getMethod) "+req.getMethod());
+		System.out.println("Error_ZUUL_Filter.run(getServletPath) "+req.getServletPath());
+		System.out.println("Error_ZUUL_Filter.run(getRequestURI) "+req.getRequestURI());
+		System.out.println("Error_ZUUL_Filter.run(getRequestURL) "+req.getRequestURL());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		System.out.println("Error_ZUUL_Filter.filterType()");
+		return FilterConstants.ERROR_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		System.out.println("Error_ZUUL_Filter.filterOrder()");
+		return 0; //high priority
+	}
+
+}
